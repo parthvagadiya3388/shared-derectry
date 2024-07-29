b0305fa0710ce5cc780172f907a175b7834e9c18
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-29 15:19:15 +0530

    input added

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -5,6 +5,8 @@ export default function MyComponents() {
     <div class="main_div" style={{ padding:'10px', textAlign: 'center', marginTop: '50px' }}>
       <div style={{ backgroundColor: 'rgb(46, 46, 160)', padding: '20px', borderRadius: '5px' }}>
         <h1 style={{ color: 'white' }}>shared-components........</h1>
+        name: <input type="text" />
+        email:<input type="text" />
       </div>
     </div>
   );
