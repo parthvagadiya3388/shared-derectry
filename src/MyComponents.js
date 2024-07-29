0610d18a09f421dfa63ce1e562972a5f00d66aff
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-29 12:49:48 +0530

    new change

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function MyComponents() {
   return (
     <div>
-        <h1>shared components2222222........................</h1>
+        <div className='container'>
+            hello
+        </div>
     </div>
   )
 }
