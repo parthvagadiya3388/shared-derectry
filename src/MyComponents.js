9f402e3499fe6f7f54663582f75424839880bb2e
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-29 13:57:53 +0530

    new line

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function MyComponents() {
   return (
     <div>
-        <div className='container bg-danger'>
-            hello
+        <div className=''>
+            <h1>shared components........</h1>
         </div>
     </div>
   )
