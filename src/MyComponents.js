bd0079a3cdf3d49c1da18e522cd1a90e5b0a37ea
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-29 14:22:26 +0530

    color added

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MyComponents() {
   return (
     <div>
-        <div className=''>
+        <div className='container bg-danger'>
             <h1>shared-components........</h1>
         </div>
     </div>
