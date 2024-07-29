29e0aab3aac166e61b46f46a1cf2ed0a23ed8824
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-29 12:53:20 +0530

    new add style

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MyComponents() {
   return (
     <div>
-        <div className='container'>
+        <div className='container bg-danger'>
             hello
         </div>
     </div>
