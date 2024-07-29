9590869f928dbb6f975f82fc578e4a5f828152ee
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-29 15:05:12 +0530

    css add

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import './style.css';
 
 export default function MyComponents() {
   return (
-    <div style={{ padding:'10px', textAlign: 'center', marginTop: '50px' }}>
+    <div class="main_div" style={{ padding:'10px', textAlign: 'center', marginTop: '50px' }}>
       <div style={{ backgroundColor: 'rgb(46, 46, 160)', padding: '20px', borderRadius: '5px' }}>
         <h1 style={{ color: 'white' }}>shared-components........</h1>
       </div>
