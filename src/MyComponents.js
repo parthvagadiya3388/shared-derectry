8f945dbd2d70496297daee8ca30147389facc585
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-07-30 10:03:00 +0530

    props added

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,14 +1,16 @@
 import './style.css';
 
-export default function MyComponents() {
+export default function MyComponents({ first_label , second_label }) {
   return (
     <div class="main_div" style={{ padding:'10px', textAlign: 'center', marginTop: '50px' }}>
       <div style={{ backgroundColor: 'rgb(46, 46, 160)', padding: '20px', borderRadius: '5px' }}>
         <h1 style={{ color: 'white' }}>shared-components........</h1>
       </div>
       <div class="input-div">
-        name: <input type="text" />
-        email:<input type="text" />
+        <label>{first_label}</label>
+        <input type="text" />
+        <label>{second_label}</label>
+        <input type="text" />
       </div>
     </div>
   );
